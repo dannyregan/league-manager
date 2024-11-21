c560371e0f631eef4e6d75101c2104fa2624f122
Danny Regan <d.regan@columbia.edu>
2024-11-21 19:57:42 +0100

    Notes file to manage things I'm learning or reviewing.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,30 +1,9 @@
-import { useState, useEffect } from "react";
-
 import Header from './header'
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
-    
-    // This is called as a side effect to a component rendering...so maybe like if the header component is rendered, fetch data from an API.
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setCounter(counter+1)
-        }, 1000)
-
-        // allows us to clean up the effect right before it rerenders the component
-        return () => {
-            clearInterval(intervalID)
-        }
-    })
-
     return (
         <div className="container">
             <Header message="League Standings"/>
-            <button onClick={() => {
-                setCounter(counter + 1);
-            }}>
-                {counter}
-            </button>
         </div>
     )
 };
