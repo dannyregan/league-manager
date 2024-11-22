12a890b7267887de30a8a0a205cfb0b51e1a9955
Danny Regan <d.regan@columbia.edu>
2024-11-22 21:01:53 +0100

    team-table component organizes the league standings.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,45 +1,56 @@
 import { ClassificationTypeNames } from 'typescript';
 import Header from './header'
+import TeamTable from "./team-table"
+import { MongoTailableCursorError } from 'mongodb';
 
 const App = ({ initialData }) => {
     return (
         <div className="container">
             <Header message="League Standings"/>
-            <div className="teamInfo">
-                <div className="rank">Rank</div>
-                <div className="teamName">Team</div>
-                <div className="wins">Wins</div>
-                <div className="losses">Losses</div>
-                <div className="points">Points</div>
-            </div>
-            <div className="team-list">
-                {initialData.teams.map((team) => {
-                    return (
-                        <div className="team-preview" key={team._id}>
-                            <div className="teamInfo">
-                                <div className="rank">{team.rank}</div>
-                                <div className="teamName">{team.teamName}</div>
-                                <div className="wins">{team.wins}</div>
-                                <div className="losses">{team.losses}</div>
-                                <div className="points">{team.points}</div>
-                            </div>
-                            <div className="players">
-                                <div className="tableHeader">
-                                    <Header message="Player Name" />
-                                    <Header message="Goals" />
-                                </div>
-                                {team.players.map((player) => (
-                                    <div className="player" key={player._id}>
-                                        <div className="playerName">{player.playerName}</div>
-                                        <div className="goals">{player.goals}</div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
+            <TeamTable teamsData={ initialData }/>
         </div>
+
+
+
+
+
+        // <div className="container">
+        //     <Header message="League Standings"/>
+        //     <div className="teamInfo">
+        //         <div className="rank">Rank</div>
+        //         <div className="teamName">Team</div>
+        //         <div className="wins">Wins</div>
+        //         <div className="losses">Losses</div>
+        //         <div className="points">Points</div>
+        //     </div>
+        //     <div className="team-list">
+        //         {initialData.teams.map((team) => {
+        //             return (
+        //                 <div className="team-preview" key={team._id}>
+        //                     <div className="teamInfo">
+        //                         <div className="rank">{team.rank}</div>
+        //                         <div className="teamName">{team.teamName}</div>
+        //                         <div className="wins">{team.wins}</div>
+        //                         <div className="losses">{team.losses}</div>
+        //                         <div className="points">{team.points}</div>
+        //                     </div>
+        //                     <div className="players">
+        //                         <div className="tableHeader">
+        //                             <Header message="Player Name" />
+        //                             <Header message="Goals" />
+        //                         </div>
+        //                         {team.players.map((player) => (
+        //                             <div className="player" key={player._id}>
+        //                                 <div className="playerName">{player.playerName}</div>
+        //                                 <div className="goals">{player.goals}</div>
+        //                             </div>
+        //                         ))}
+        //                     </div>
+        //                 </div>
+        //             );
+        //         })}
+        //     </div>
+        // </div>
     );
 };
 
---
+++ src/components/team-table.tsx
@@ -0,0 +1,38 @@
+const TeamTable = ({ teamsData }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">Rank</th>
+                    <th scope="col" className="teamName">Team</th>
+                    <th scope="col">W</th>
+                    <th scope="col">D</th>
+                    <th scope="col">L</th>
+                    <th scope="col">P%</th>
+                    <th scope="col">PF</th>
+                    <th scope="col">PA</th>
+                    <th scope="col">LP</th>
+                </tr>
+            </thead>
+            <tbody>
+                {teamsData.teams.map((team) => {
+                    return (
+                        <tr key={team._id}>
+                            <th scope="row">{team.rank}</th>
+                            <td className="teamName">{team.teamName}</td>
+                            <td>{team.wins}</td>
+                            <td>{team.draws}</td>
+                            <td>{team.losses}</td>
+                            <td>{team.percent}%</td>
+                            <td>{team.pointsFor}</td>
+                            <td>{team.pointsAgainst}</td>
+                            <td className="leaguePoints">{team.leaguePoints}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+export default TeamTable
