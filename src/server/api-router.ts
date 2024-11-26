be2489434b6b5a6bd54f4c793d8f09f0238ae07d
Danny Regan <d.regan@columbia.edu>
2024-11-26 16:04:58 +0100

    Access team-specific info when searched for directly in the URL.

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -9,16 +9,22 @@ router.use(cors());
 router.get("/teams", async (req, res) => {
     const client = await connectClient();
 
-    const teams = await client.collection("teams")
+    // Fetches the api data and searches in the "teams" collection. It gives us back an array of the teams.
+    const teams = await client
+        .collection("teams")
         .find()
         .toArray();
-
     res.send({ teams });
 });
 
-router.get("/teams/:teamId", (req, res) => {
-    console.log(req.params.teamId);
+router.get("/team/:teamID", async (req, res) => {
+    const client = await connectClient();
 
+    const team = await client
+        .collection("teams")
+        .findOne({ id: req.params.teamID })
+
+        res.send({ team })
 });
 
 export default router
